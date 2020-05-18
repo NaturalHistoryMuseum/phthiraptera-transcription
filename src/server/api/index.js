155bdaed649034136dd5416d055e68aa71889ca5
Paul Kiddle <p.kiddle@nhm.ac.uk>
2020-05-18 15:41:47 +0100

    Allow default oauth url on prod

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -3,7 +3,7 @@ const OrcidAuth = require('./orcid');
 const port = getenv('PORT', '1234');
 const host = getenv('HOST', `http://localhost:${port}`);
 const redirectUri = `${host}/oauth`;
-const oauthUrl = getenv('OAUTH_URL', 'https://orcid.org/oauth');
+const oauthUrl = process.env.OAUTH_URL || 'https://orcid.org/oauth';
 
 const orcid = new OrcidAuth({
   clientId: getenv('ORCID_CLIENT_ID', ''),
